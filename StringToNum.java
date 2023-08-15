021258b4cfa214f0545c99891239d59293489daf
AjithkumarRamesh <rohitajith007@gmail.com>
2023-08-15 15:25:38 +0530

    Updates

--- StringToNum.java
+++ StringToNum.java
@@ -2,6 +2,14 @@ package peparationQuestions;
 
 public class StringToNum {
 	public static void main(String[] args) {
+		String words="a1b2c3";
+		for(int i=0;i<words.length();i+=2) {
+			int c=Integer.parseInt(words.charAt(i+1)+"");
+			for(int j=0;j<c;j++) {
+				System.out.print(words.charAt(i)+" ");
+			}
+			System.out.println();
+		}
 		
 	}
 
